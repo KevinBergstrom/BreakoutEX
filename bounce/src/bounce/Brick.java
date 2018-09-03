393a64ce14c9c560daf12eafa942c482a24b694a
kevbergstrom <kevin.bergstrom@wsu.edu>
2018-09-03 14:36:12 -0700

    Added Brick class and created brick holder in BounceGame

---
+++ bounce/src/bounce/Brick.java
@@ -0,0 +1,92 @@
+package bounce;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Image;
+
+import jig.Entity;
+import jig.ResourceManager;
+
+
+ class Brick extends Entity {
+
+	private Image brickImg;
+	private int bricksX;
+	private int bricksY;
+	private Color brickColor;
+	private int maxHealth;
+	private int health;
+
+	public Brick(final float x, final float y, final int bx, final int by, Color col) {
+		super(x, y);
+		//bx and by respresent how many base bricks long or wide it is (1 to 4)
+		//addImageWithBoundingBox(ResourceManager.getImage(BounceGame.BALL_BALLIMG_RSC));
+		Image image = getBrickImageFromSize(bx, by);
+		brickImg = image.getScaledCopy(bx*5, by*5);
+		brickImg.setFilter(Image.FILTER_NEAREST);
+		brickImg.setImageColor(col.r, col.g, col.b);
+		addImageWithBoundingBox(brickImg);
+		
+		maxHealth = bx*by;
+		health = bx*by;
+		
+		bricksX = bx;
+		bricksY = by;
+		brickColor = col;
+	}
+	
+	public Image getBrickImageFromSize(int bx, int by) {
+		
+		if(bx==1 && by == 1) {
+			return ResourceManager.getImage(BounceGame.BRICK_1X1_RSC);
+		}else if(bx==1 && by == 2) {
+			return ResourceManager.getImage(BounceGame.BRICK_1X2_RSC);
+		}else if(bx==1 && by == 3) {
+			return ResourceManager.getImage(BounceGame.BRICK_1X3_RSC);
+		}else if(bx==1 && by == 4) {
+			return ResourceManager.getImage(BounceGame.BRICK_1X4_RSC);
+		}else if(bx==2 && by == 1) {
+			return ResourceManager.getImage(BounceGame.BRICK_2X1_RSC);
+		}else if(bx==3 && by == 1) {
+			return ResourceManager.getImage(BounceGame.BRICK_3X1_RSC);
+		}else if(bx==4 && by == 1) {
+			return ResourceManager.getImage(BounceGame.BRICK_4X1_RSC);
+		}else if(bx==2 && by == 2) {
+			return ResourceManager.getImage(BounceGame.BRICK_2X2_RSC);
+		}else if(bx==3 && by == 3) {
+			return ResourceManager.getImage(BounceGame.BRICK_3X3_RSC);
+		}else {
+			//wrong size
+			return null;
+		}
+	}
+
+	public void damageBrick(int damage) {
+		health -= damage;
+		this.removeImage(brickImg);
+		if(health<0) {
+			health = 0;
+			onDeath();
+		}else {
+			float brickAlpha = health/maxHealth;
+			Image image = getBrickImageFromSize(bricksX, bricksY);
+			brickImg = image.getScaledCopy(bricksX*5, bricksY*5);
+			brickImg.setFilter(Image.FILTER_NEAREST);
+			brickImg.setImageColor(brickColor.r, brickColor.g, brickColor.b,brickAlpha);
+			addImageWithBoundingBox(brickImg);
+			onHit();
+		}
+		
+	}
+	
+	public void onHit() {
+		
+	}
+	
+	public void onDeath() {
+		
+	}
+
+	/*public void update(final int delta) {
+		
+	}*/
+}
